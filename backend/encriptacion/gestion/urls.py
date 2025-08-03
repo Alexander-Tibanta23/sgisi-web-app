674cf845ffdff3f852e0c52278afc64753563341
Monica Lucio <monica.lucio@saviasoft.com>
2025-08-02 22:22:55 -0500

    Elimino carpeta encriptacion de backend

--- backend/encriptacion/gestion/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('subir_evidencia/', views.subir_evidencia, name='subir_evidencia'),
-    # Puedes agregar otras rutas aquí después
-]
